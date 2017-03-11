edaacf8be6ff052aa6346954b06b962335ed83b7
Sidd10 <awatramanisiddharth24@gmail.com>
2017-03-11 14:58:43 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+﻿var i = 0;
+var a = true;
+document.addEventListener("DOMContentLoaded",
+    function (event)
+    {
+        function sayHello(event)
+        {
+            document.getElementById("label").innerHTML = (i += 1);
+
+            this.textContent = "Said it!";
+            var name = document.getElementById("name").value;
+            var message = "<h2>Hello " + name + "!</h2>";
+
+            document.getElementById("content").innerHTML = message;
+
+            if (name === "student")
+            {
+                if (a == true)
+                {
+                    var title = document.querySelector("#title").textContent;
+                    title += " & Lovin' it!";
+                    document.querySelector("h1").textContent = title;
+                    a = false;
+                }  
+            } 
+        }
+      // Unobtrusive event binding
+      document.querySelector("button").addEventListener("click", sayHello);
+  });
